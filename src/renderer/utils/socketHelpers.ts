1cb4a7735bfefd673c45a84b93891e07f57f6a7c
ahwelgemoed <arno.welgemoed@outlook.com>
2021-02-10 16:24:36 +0100

    .

--- src/renderer/utils/socketHelpers.ts
+++ src/renderer/utils/socketHelpers.ts
@@ -1,80 +1,80 @@
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useRef, useState } from "react";
 // import socketIOClient from "socket.io-client";
-import socketIOClient from 'socket.io-client'
-import { createStandaloneToast } from '@chakra-ui/react'
+import socketIOClient from "socket.io-client";
+import { createStandaloneToast } from "@chakra-ui/react";
 
-import { socketMessage } from '../../socketMessages'
+import { socketMessage } from "../../socketMessages";
 
-const toast = createStandaloneToast()
+const toast = createStandaloneToast();
 
-const SOCKET_SERVER_URL = 'http://127.0.0.1:7891'
+const SOCKET_SERVER_URL = "http://10.211.55.4:7891";
 
 export const useSocket = () => {
-  const [socketProjects, setSocketProjects] = useState()
+  const [socketProjects, setSocketProjects] = useState();
   const socketRef = useRef<SocketIOClient.Socket>(
     socketIOClient(SOCKET_SERVER_URL)
-  )
+  );
 
   useEffect(() => {
     //   @ts-ignore
-    socketRef.current = socketIOClient(SOCKET_SERVER_URL)
+    socketRef.current = socketIOClient(SOCKET_SERVER_URL);
 
-    socketRef.current.on('connect', () => {
-      console.log(socketRef.current.id)
-    })
+    socketRef.current.on("connect", () => {
+      console.log(socketRef.current.id);
+    });
     socketRef.current.on(socketMessage.ALL_PROJECTS, (message: any) => {
-      setSocketProjects(message)
-    })
+      setSocketProjects(message);
+    });
 
     return () => {
       //   @ts-ignore
-      socketRef.current.disconnect()
-    }
-  }, [])
+      socketRef.current.disconnect();
+    };
+  }, []);
   useEffect(() => {
     if (socketRef.current) {
       // @ts-ignore
       if (socketRef.current.connected) {
         toast({
-          title: 'Windows Connected',
-          status: 'success',
+          title: "Windows Connected",
+          status: "success",
           duration: 7000,
-          position: 'top',
-          isClosable: true
-        })
+          position: "top",
+          isClosable: true,
+        });
       }
     }
-  }, [socketRef])
+  }, [socketRef]);
 
   const sendProjects = (messageBody: any) => {
     socketRef.current.compress(false).emit(socketMessage.ALL_PROJECTS, {
-      messageBody: stringMyBody(messageBody)
-    })
-  }
+      messageBody: stringMyBody(messageBody),
+    });
+  };
   const sendOpenStudioInProject = (messageBody: any) => {
     socketRef.current.emit(socketMessage.OPEN_IN_STUDIO, {
       body: stringMyBody(messageBody),
-      senderId: socketRef.current.id
-    })
-  }
+      senderId: socketRef.current.id,
+    });
+  };
   const sendOpenInVsCode = (messageBody: any) => {
     socketRef.current.emit(socketMessage.OPEN_IN_VSCODE, {
       body: stringMyBody(messageBody),
-      senderId: socketRef.current.id
-    })
-  }
+      senderId: socketRef.current.id,
+    });
+  };
 
   const isSocketConnected =
-    socketRef && socketRef.current && socketRef.current.connected
+    socketRef && socketRef.current && socketRef.current.connected;
 
   return {
     isSocketConnected,
     sendOpenInVsCode,
     sendProjects,
     sendOpenStudioInProject,
-    socketProjects
-  }
-}
+    socketProjects,
+  };
+};
 const stringMyBody = (body: any) => {
-  return JSON.stringify(body)
-}
+  return JSON.stringify(body);
+};
